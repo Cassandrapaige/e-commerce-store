d3f047fabee6f4b26f5a5fc73fee516f760994d1
Cassandra Rossall <cassandrarossall@gmail.com>
2020-05-03 15:52:47 +1200

    fixed bug when removing item from cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,19 +14,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 }
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
-    const exisitingCartItem = cartItems.find(
-        cartItem => cartItem.id === cartItemToRemove.id && cartItem.size === cartItemToRemove.size
-    )
-
-    if(exisitingCartItem.quantity === 1) {
-        return cartItems.filter(cartItem => cartItem !== exisitingCartItem)
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToRemove.id && cartItem.size === cartItemToRemove.size
+    );
+  
+    if (existingCartItem.quantity === 1) {
+        window.location.reload();
+        return cartItems.filter(cartItem => cartItem !== cartItemToRemove);
     }
-
-    return cartItems.map(cartItem => cartItem === exisitingCartItem ? 
-            { ...cartItem, quantity: cartItem.quantity - 1}
-            : cartItem
-        ) 
-}
+  
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToRemove.id && cartItem.size === cartItemToRemove.size
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+  };
 
 export const clearItemFromCart = (cartItems, cartItemsToClear) => {
     const exisitingCartItem = cartItems.find(
